0680279362332ebeac8020a9324ea21a64967fa8
EnderPrism <76617157+enderprism@users.noreply.github.com>
2024-10-21 21:50:32 +0200

    fix.graphics: rebound sprite factor lowering when the player is falling

--- src/interactables/public_components/ReboundComponent.gd
+++ src/interactables/public_components/ReboundComponent.gd
@@ -3,13 +3,15 @@ class_name ReboundComponent
 
 var _velocity: float
 var sprite: Node2D
+var player_distance: float
 
 func _physics_process(delta: float) -> void:
 	var player_velocity := LevelManager.player.velocity.rotated(-LevelManager.player.gameplay_rotation) \
 			* LevelManager.player.gravity_multiplier
 	if parent.global_position.distance_to(LevelManager.player.global_position) > Player.TERMINAL_VELOCITY.y * 2 * delta:
 		_velocity = player_velocity.y
-	var player_distance := (parent.global_position.rotated(-LevelManager.player.gameplay_rotation).y) \
+	if player_velocity.y < 0:
+		player_distance = (parent.global_position.rotated(-LevelManager.player.gameplay_rotation).y) \
 			- LevelManager.player.global_position.rotated(-LevelManager.player.gameplay_rotation).y \
 			* LevelManager.player.gravity_multiplier
 	sprite.factor = clampf(player_distance/700, 0, 1)
