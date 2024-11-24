4ba4f186c7422fe414055e6653d2a81eabcd2ab9
EnderPrism <76617157+enderprism@users.noreply.github.com>
2024-11-24 19:37:18 +0100

    fix: ground objects rotating in freefly

--- src/GroundObject.gd
+++ src/GroundObject.gd
@@ -7,19 +7,15 @@ const LERP_FACTOR: float = 30
 @export var DEFAULT_Y: float
 @export_enum("Up:-1", "Down:1") var ground_position: int = 1
 
+var _previous_camera_rotation: float
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if LevelManager.player_camera != null:
 		var zoom_factor: Vector2 = PlayerCamera.DEFAULT_ZOOM/LevelManager.player_camera.zoom
 		# var zoom_factor := Vector2.ONE
-		var global_position_rotated: Vector2 = global_position.rotated(LevelManager.player.gameplay_rotation)
-		if LevelManager.player_camera._freefly:
-			global_position_rotated = global_position_rotated.lerp(
-				Vector2(global_position_rotated.x, DEFAULT_Y),
-				1-exp(-delta * LERP_FACTOR)
-			)
-		else:
+		if not LevelManager.player_camera._freefly:
+			var global_position_rotated: Vector2 = global_position.rotated(LevelManager.player.gameplay_rotation)
 			global_position_rotated = global_position_rotated.lerp(
 				Vector2(
 					GroundData.center.x,
@@ -29,7 +25,14 @@ func _process(delta: float) -> void:
 				),
 				1-exp(-delta * LERP_FACTOR)
 			)
-		global_position = global_position_rotated.rotated(-LevelManager.player.gameplay_rotation)
+			global_position = global_position_rotated.rotated(-LevelManager.player.gameplay_rotation)
+		else:
+			global_position = global_position.lerp(
+				Vector2(global_position.x, DEFAULT_Y),
+				1-exp(-delta * LERP_FACTOR)
+			)
+			pass
+		_previous_camera_rotation = LevelManager.player_camera.rotation
 
 ## Sync the rotation with P1's [member Player.gameplay_rotation] on gamemode change.
 func _sync_rotation(new_gameplay_rotation: float) -> void:
