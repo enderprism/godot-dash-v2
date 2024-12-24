bd0720f094ae56d01264f425163f29158aa53333
EnderPrism <76617157+enderprism@users.noreply.github.com>
2024-12-25 00:26:36 +0100

    fix: selected objects keep selection color when playtesting and after playtesting

--- src/EditorScene.gd
+++ src/EditorScene.gd
@@ -86,6 +86,10 @@ func _on_playtest_pressed() -> void:
 		%EditorModes.hide()
 		%SidePanel.hide()
 		%EditorViewport.mouse_filter = MOUSE_FILTER_STOP
+		if not $EditHandler.selection.is_empty():
+			$EditHandler.selection.map(func(object): object.get_node("SelectionHighlight").queue_free())
+			await $EditHandler.selection[-1].get_node("SelectionHighlight").tree_exited
+			$EditHandler.selection.clear()
 		LevelManager.editor_level_backup.pack(level)
 		LevelManager.editor_backup.pack(self)
 		$GameScene._start_level()
