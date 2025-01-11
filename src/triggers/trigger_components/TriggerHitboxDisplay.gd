1b5756b147040a9d8e6dd3ba3ef3455ca7a5bfe4
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-01-11 15:50:01 +0100

    fix: stretched triggerhitboxdisplay when the hitbox is a square

--- src/triggers/trigger_components/TriggerHitboxDisplay.gd
+++ src/triggers/trigger_components/TriggerHitboxDisplay.gd
@@ -5,15 +5,18 @@ class_name TriggerHitboxDisplay
 
 var shape: Rect2
 
+func _ready() -> void:
+	global_scale = Vector2.ONE
 
 func update_shape(hitbox_shape: TriggerBase.TriggerHitboxShape, hitbox_position: Vector2, hitbox_height: float) -> void:
+	global_scale = Vector2.ONE
 	match hitbox_shape:
 		TriggerBase.TriggerHitboxShape.LINE:
-			show()
+			visible = parent.sprite_visible()
 			shape.size = Vector2(2.0, -hitbox_height * LevelManager.CELL_SIZE * 2)
 		TriggerBase.TriggerHitboxShape.SQUARE:
-			show()
-			shape.size = Vector2.ONE * LevelManager.CELL_SIZE
+			visible = parent.sprite_visible()
+			shape.size = Vector2.ONE * LevelManager.CELL_SIZE * parent.global_scale
 		TriggerBase.TriggerHitboxShape.DISABLED:
 			hide()
 	shape.position = hitbox_position - shape.size * 0.5
