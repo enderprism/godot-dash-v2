709cbb7038b15f21fe5f4512377b1fcbe167d004
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-02-27 11:27:09 +0100

    enhancement: move trigger move towards distance multiplier; improve export ui

--- src/triggers/MoveTrigger.gd
+++ src/triggers/MoveTrigger.gd
@@ -5,7 +5,7 @@ class_name MoveTrigger
 enum Mode {
 	SET,
 	ADD,
-	COPY,
+	MOVE_TOWARDS,
 }
 
 @export var mode: Mode = Mode.ADD:
@@ -14,8 +14,16 @@ enum Mode {
 		notify_property_list_changed()
 @export var _set_position: Vector2
 @export var _add_position: Vector2
-@export var _copy_target: Node2D
-@export var _copy_offset: Vector2 ## Offset in global coordinates from the move target.
+@export_group("Move Towards", "_move_towards")
+@export var _move_towards_target: Node2D
+## Multiplies the target distance between each object in the group. [br][br]
+## [b]Examples:[/b] [br]
+## - [code]0.0[/code]: the group's objects will all move towards the target object. [br]
+## - [code]1.0[/code]: the group's objects will follow the target object but [b]keep[/b] their relative distance to it. [br]
+## - [code]2.0[/code]: the group's objects will follow the target object but [b]double[/b] their relative distance to it. [br]
+## - [code]-1.0[/code]: the group's objects will follow the target object but [b]invert[/b] their relative distance to it.
+@export_range(0.0, 2.0, 0.05, "or_greater", "or_less") var _move_towards_distance_multiplier: float
+@export var _move_towards_offset: Vector2 ## Offset in global coordinates from the move target.
 
 # Hide unneeded elements in the inspector
 func _validate_property(property: Dictionary) -> void:
@@ -23,7 +31,7 @@ func _validate_property(property: Dictionary) -> void:
 		property.usage = PROPERTY_USAGE_NO_EDITOR
 	if property.name == "_add_position" and mode != Mode.ADD:
 		property.usage = PROPERTY_USAGE_NO_EDITOR
-	if property.name in ["_copy_target", "_copy_offset"] and mode != Mode.COPY:
+	if property.name in ["_move_towards_target", "_move_towards_offset", "_move_towards_distance_multiplier"] and mode != Mode.MOVE_TOWARDS:
 		property.usage = PROPERTY_USAGE_NO_EDITOR
 
 var base: TriggerBase
@@ -32,28 +40,33 @@ var target_link: TargetLink
 
 var _targets: Array[Node]
 var _initial_positions: Dictionary
+var _initial_distances: Dictionary
 
 func _ready() -> void:
 	TriggerSetup.setup(self, TriggerSetup.ADD_TARGET_LINK | TriggerSetup.ADD_EASING)
 	base.sprite.set_texture(preload("res://assets/textures/triggers/Move.svg"))
 	_targets = get_tree().get_nodes_in_group(base.target_group)
 
-func _process(_delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if not Engine.is_editor_hint() and not is_zero_approx(easing._weight):
 		if not _targets.is_empty():
 			var _weight_delta = easing.get_weight_delta()
 			for _target in _targets:
 				var _initial_global_position = _initial_positions[_target]
+				var _initial_distance = _initial_distances[_target]
 				match mode:
 					Mode.SET:
 						_target.global_position += (owner.to_global(_set_position) - _initial_global_position) * _weight_delta
 					Mode.ADD:
 						_target.global_position += _add_position * _weight_delta
-					Mode.COPY:
-						if _copy_target != null:
-							_target.global_position = lerp(_initial_global_position, _copy_target.global_position + _copy_offset, easing._weight)
+					Mode.MOVE_TOWARDS:
+						if _move_towards_target != null:
+							_target.global_position = lerp(
+									_initial_global_position,
+									_move_towards_target.global_position + (_initial_distance * _move_towards_distance_multiplier) + _move_towards_offset,
+									easing._weight)
 						elif LevelManager.in_editor and LevelManager.level_playing:
-							printerr("In ", name, ": copy_target is unset!")
+							printerr("In ", name, ": move_towards_target is unset!")
 		elif LevelManager.in_editor and LevelManager.level_playing:
 			printerr("In ", name, ": _target is unset")
 	elif Engine.is_editor_hint() or LevelManager.in_editor:
@@ -68,6 +81,8 @@ func start(_body: Node2D) -> void:
 		if not _targets.is_empty():
 			for _target in _targets:
 				_initial_positions[_target] = _target.global_position
+				if mode == Mode.MOVE_TOWARDS:
+					_initial_distances[_target] = _move_towards_target.global_position - _target.global_position
 		elif LevelManager.in_editor and LevelManager.level_playing:
 			printerr("In ", name, ": base._target is unset")
 
