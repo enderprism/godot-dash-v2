95fac8349987c9e191a4a58e7f09b0f4bbeae4d2
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-04-01 21:10:48 +0200

    feat: triggeruihandler single usage

--- src/editor/TriggerUIHandler.gd
+++ src/editor/TriggerUIHandler.gd
@@ -2,40 +2,50 @@ extends Node
 class_name TriggerUIHandler
 
 @export var trigger_editor: TriggerEditor
-@export var multi_activate: Property
+@export var single_usage: Property
 @export var spawn_triggered: Property
 @export var touch_triggered: Property
 
 func _on_edit_handler_selection_changed(selection:Array[Node2D]) -> void:
 	if not is_selection_trigger_only(selection):
 		return
-	var trigger: TriggerBase = selection[0].get_node("TriggerBase")
-	_on_spawn_triggered_value_changed(trigger._hitbox_shape == TriggerBase.TriggerHitboxShape.DISABLED)
-	_on_touch_triggered_value_changed(trigger._hitbox_shape == TriggerBase.TriggerHitboxShape.SQUARE)
-	var trigger_bases: Array[TriggerBase] = selection.map(func(object): return object.get_node("TriggerBase") as TriggerBase)
+	var trigger_bases: Array = selection.map(func(object): return object.get_node("TriggerBase") as TriggerBase)
+	var trigger_base := trigger_bases[0] as TriggerBase
+	single_usage.set_value(trigger_base.single_usage, Property.Type.BOOL)
+	spawn_triggered.set_value(trigger_base._hitbox_shape == TriggerBase.TriggerHitboxShape.DISABLED, Property.Type.BOOL)
+	touch_triggered.set_value(trigger_base._hitbox_shape == TriggerBase.TriggerHitboxShape.SQUARE, Property.Type.BOOL)
+	spawn_triggered.set_input_state(trigger_base._hitbox_shape != TriggerBase.TriggerHitboxShape.SQUARE)
+	touch_triggered.set_input_state(trigger_base._hitbox_shape != TriggerBase.TriggerHitboxShape.DISABLED)
+	for property in [single_usage, spawn_triggered, touch_triggered]:
+		property.value_changed.get_connections().map(func(connection): property.value_changed.disconnect(connection.callable))
+	single_usage.value_changed.connect(_on_single_usage_value_changed.bind(trigger_bases))
 	spawn_triggered.value_changed.connect(_on_spawn_triggered_value_changed.bind(trigger_bases))
 	touch_triggered.value_changed.connect(_on_touch_triggered_value_changed.bind(trigger_bases))
 	if len(selection) != 1:
 		return
-	trigger_editor.build_ui(trigger)
+	trigger_editor.build_ui(trigger_base)
 
 
-func _on_spawn_triggered_value_changed(value: bool, triggers: Array = []) -> void:
+func _on_single_usage_value_changed(value: bool, trigger_bases: Array) -> void:
+	trigger_bases.map(func(trigger_base): trigger_base.single_usage = value)
+
+
+func _on_spawn_triggered_value_changed(value: bool, trigger_bases: Array) -> void:
 	touch_triggered.set_input_state(not value)
-	if triggers.is_empty():
+	if trigger_bases.is_empty():
 		return
 	var make_trigger_spawn_triggered := func(trigger_base: TriggerBase):
 		trigger_base._hitbox_shape = TriggerBase.TriggerHitboxShape.DISABLED if value else TriggerBase.TriggerHitboxShape.LINE
-	triggers.map(make_trigger_spawn_triggered)
+	trigger_bases.map(make_trigger_spawn_triggered)
 
 
-func _on_touch_triggered_value_changed(value:Variant, triggers: Array = []) -> void:
+func _on_touch_triggered_value_changed(value:Variant, trigger_bases: Array) -> void:
 	spawn_triggered.set_input_state(not value)
-	if triggers.is_empty():
+	if trigger_bases.is_empty():
 		return
 	var make_trigger_spawn_triggered := func(trigger_base: TriggerBase):
 		trigger_base._hitbox_shape = TriggerBase.TriggerHitboxShape.SQUARE if value else TriggerBase.TriggerHitboxShape.LINE
-	triggers.map(make_trigger_spawn_triggered)
+	trigger_bases.map(make_trigger_spawn_triggered)
 
 
 static func is_selection_trigger_only(selection: Array[Node2D]) -> bool:
