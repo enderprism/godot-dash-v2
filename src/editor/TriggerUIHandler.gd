a0d3556361571d1d30b05ac416f0b872bf17823c
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-04-01 20:54:12 +0200

    feat: triggeruihandler modify trigger hitbox shape

--- src/editor/TriggerUIHandler.gd
+++ src/editor/TriggerUIHandler.gd
@@ -7,18 +7,35 @@ class_name TriggerUIHandler
 @export var touch_triggered: Property
 
 func _on_edit_handler_selection_changed(selection:Array[Node2D]) -> void:
-	if not (len(selection) == 1 and is_selection_trigger_only(selection)):
+	if not is_selection_trigger_only(selection):
 		return
 	var trigger: TriggerBase = selection[0].get_node("TriggerBase")
+	_on_spawn_triggered_value_changed(trigger._hitbox_shape == TriggerBase.TriggerHitboxShape.DISABLED)
+	_on_touch_triggered_value_changed(trigger._hitbox_shape == TriggerBase.TriggerHitboxShape.SQUARE)
+	var trigger_bases: Array[TriggerBase] = selection.map(func(object): return object.get_node("TriggerBase") as TriggerBase)
+	spawn_triggered.value_changed.connect(_on_spawn_triggered_value_changed.bind(trigger_bases))
+	touch_triggered.value_changed.connect(_on_touch_triggered_value_changed.bind(trigger_bases))
+	if len(selection) != 1:
+		return
 	trigger_editor.build_ui(trigger)
 
 
-func _on_spawn_triggered_value_changed(value: bool) -> void:
+func _on_spawn_triggered_value_changed(value: bool, triggers: Array = []) -> void:
 	touch_triggered.set_input_state(not value)
+	if triggers.is_empty():
+		return
+	var make_trigger_spawn_triggered := func(trigger_base: TriggerBase):
+		trigger_base._hitbox_shape = TriggerBase.TriggerHitboxShape.DISABLED if value else TriggerBase.TriggerHitboxShape.LINE
+	triggers.map(make_trigger_spawn_triggered)
 
 
-func _on_touch_triggered_value_changed(value:Variant) -> void:
+func _on_touch_triggered_value_changed(value:Variant, triggers: Array = []) -> void:
 	spawn_triggered.set_input_state(not value)
+	if triggers.is_empty():
+		return
+	var make_trigger_spawn_triggered := func(trigger_base: TriggerBase):
+		trigger_base._hitbox_shape = TriggerBase.TriggerHitboxShape.SQUARE if value else TriggerBase.TriggerHitboxShape.LINE
+	triggers.map(make_trigger_spawn_triggered)
 
 
 static func is_selection_trigger_only(selection: Array[Node2D]) -> bool:
