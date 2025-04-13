89f805d062519cefdd94c37ad02a092b2a087144
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-04-13 19:41:45 +0200

    vector2spinbox refactor

--- src/gui/Vector2SpinBox.gd
+++ src/gui/Vector2SpinBox.gd
@@ -11,40 +11,31 @@ signal value_changed(new_value: Vector2)
 @export var max_value: float
 @export var allow_greater: bool
 @export var allow_lesser: bool
-@export var prefix: String:
-	set(value):
-		prefix = value
-		if value != "":
-			for spinbox in [spinbox_x, spinbox_y]:
-				spinbox.prefix = value
-		else:
-			spinbox_x.prefix = "x:"
-			spinbox_y.prefix = "y:"
+@export var prefix: String: set = _set_prefix
 @export var suffix: String:
 	set(value):
 		suffix = value
 		for spinbox in [spinbox_x, spinbox_y]:
 			spinbox.suffix = value
-
+@export var select_all_on_focus: bool
 @onready var spinbox_x: SpinBox
 @onready var spinbox_y: SpinBox
 var aspect_ratio: float
-var value: Vector2:
-	set(new_value):
-		value = new_value
-		if spinbox_x == null:
-			spinbox_x = NodeUtils.get_node_or_add(self, "SpinBoxX", SpinBox, NodeUtils.INTERNAL | NodeUtils.SET_OWNER)
-		if spinbox_y == null:
-			spinbox_y = NodeUtils.get_node_or_add(self, "SpinBoxY", SpinBox, NodeUtils.INTERNAL | NodeUtils.SET_OWNER)
-		if not Engine.is_editor_hint():
-			spinbox_x.set_value_no_signal(new_value.x)
-			spinbox_y.set_value_no_signal(new_value.y)
-		value_changed.emit(value)
+var value: Vector2: set = _set_value
 
 
 func _ready() -> void:
 	spinbox_x = NodeUtils.get_node_or_add(self, "SpinBoxX", SpinBox, NodeUtils.INTERNAL)
 	spinbox_y = NodeUtils.get_node_or_add(self, "SpinBoxY", SpinBox, NodeUtils.INTERNAL)
+	spinbox_x.value_changed.connect(_set_x)
+	spinbox_y.value_changed.connect(_set_y)
+	update_internals()
+	_set_value(value)
+
+
+func update_internals() -> void:
+	if keep_aspect:
+		aspect_ratio = get_viewport_rect().size.x/get_viewport_rect().size.y
 	for spinbox in [spinbox_x, spinbox_y]:
 		spinbox.min_value = min_value
 		spinbox.max_value = max_value
@@ -55,32 +46,47 @@ func _ready() -> void:
 		spinbox.custom_minimum_size.x = 128
 		spinbox.alignment = HORIZONTAL_ALIGNMENT_FILL
 		spinbox.rounded = rounded
-	spinbox_x.prefix = "x: "
-	spinbox_y.prefix = "y: "
-	if not Engine.is_editor_hint():
-		spinbox_x.value_changed.connect(_set_x)
-		spinbox_y.value_changed.connect(_set_y)
-	if keep_aspect:
-		aspect_ratio = get_viewport_rect().size.x/get_viewport_rect().size.y
-	value = value
+		spinbox.select_all_on_focus = select_all_on_focus
+	_set_prefix(prefix)
 
+
+func _set_prefix(new_prefix: String) -> void:
+	prefix = new_prefix
+	if new_prefix != "":
+		for spinbox in [spinbox_x, spinbox_y]:
+			spinbox.prefix = value
+	else:
+		spinbox_x.prefix = "x:"
+		spinbox_y.prefix = "y:"
+
+func _set_value(new_value: Vector2) -> void:
+	value = new_value
+	if spinbox_x == null:
+		spinbox_x = NodeUtils.get_node_or_add(self, "SpinBoxX", SpinBox, NodeUtils.INTERNAL | NodeUtils.SET_OWNER)
+	if spinbox_y == null:
+		spinbox_y = NodeUtils.get_node_or_add(self, "SpinBoxY", SpinBox, NodeUtils.INTERNAL | NodeUtils.SET_OWNER)
+	if not Engine.is_editor_hint():
+		spinbox_x.set_value_no_signal(new_value.x)
+		spinbox_y.set_value_no_signal(new_value.y)
+	value_changed.emit(value)
 
 func _set_x(new_value: float) -> void:
 	value.x = new_value
+	value_changed.emit(value)
 	if keep_aspect:
 		value.y = new_value * 1/aspect_ratio
 		spinbox_y.set_value_no_signal(value.y)
-		value_changed.emit(value)
 
 
 func _set_y(new_value: float) -> void:
 	value.y = new_value
+	value_changed.emit(value)
 	if keep_aspect:
 		value.x = new_value * aspect_ratio
 		spinbox_x.set_value_no_signal(value.x)
-		value_changed.emit(value)
 
 func set_value_no_signal(new_value: Vector2):
 	spinbox_x.set_value_no_signal(new_value.x)
 	spinbox_y.set_value_no_signal(new_value.y)
+	print_debug(new_value, ": ", spinbox_x.value, ", ", spinbox_y.value)
 
