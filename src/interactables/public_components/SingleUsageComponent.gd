290b690c347c4a80649e494fbf90c9484c1f9cd4
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-04-18 23:03:24 +0200

    enhancement: singleusagecomponent: use component parent

--- src/interactables/public_components/SingleUsageComponent.gd
+++ src/interactables/public_components/SingleUsageComponent.gd
@@ -1,22 +1,21 @@
 extends Component
 class_name SingleUsageComponent
 
-@onready var _parent := get_parent() as Area2D
-var _enabled := false
+var enabled := false
 
 
 func _ready() -> void:
 	super()
-	_enabled = _parent.single_usage
-	if _enabled:
-		if _parent is OrbInteractable:
-			_parent.pressed.connect(disable)
+	enabled = parent.single_usage
+	if enabled:
+		if parent is OrbInteractable:
+			parent.pressed.connect(disable)
 		else:
-			_parent.body_entered.connect(disable)
+			parent.body_entered.connect(disable)
 
 
 func disable(_body: Node2D) -> void:
-	_parent.set_deferred("monitorable", false)
-	_parent.set_deferred("monitoring", false)
-	if _parent is TriggerBase:
-		_parent.set_deferred("process_mode", PROCESS_MODE_DISABLED)
+	parent.set_deferred("monitorable", false)
+	parent.set_deferred("monitoring", false)
+	if parent is TriggerBase:
+		parent.set_deferred("process_mode", PROCESS_MODE_DISABLED)
