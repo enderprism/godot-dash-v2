1e33ec811fc3f81fd962e67189fffe85fe8596a2
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-04-18 19:06:25 +0200

    fix: interactables flood scene tree with editorselectioncolliders

--- src/interactables/Interactable.gd
+++ src/interactables/Interactable.gd
@@ -7,10 +7,12 @@ var components: Array[Component]
 
 func _ready() -> void:
 	if LevelManager.in_editor:
-		var editor_selection_collider := preload("res://scenes/components/level_components/EditorSelectionCollider.tscn").instantiate() as EditorSelectionCollider
-		editor_selection_collider.type = EditorSelectionCollider.Type.INTERACTABLE
-		add_child(editor_selection_collider)
-		editor_selection_collider.set_owner(self)
+		var editor_selection_collider := get_node_or_null("EditorSelectionCollider")
+		if editor_selection_collider == null:
+			editor_selection_collider = preload("res://scenes/components/level_components/EditorSelectionCollider.tscn").instantiate() as EditorSelectionCollider
+			editor_selection_collider.type = EditorSelectionCollider.Type.INTERACTABLE
+			add_child(editor_selection_collider)
+			editor_selection_collider.set_owner(self)
 	if has_node("Hitbox"):
 		$Hitbox.debug_color = Color("00ff0033")
 
