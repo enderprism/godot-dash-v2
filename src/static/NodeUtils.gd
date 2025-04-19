2309fd7eb1d61546ecbc0a465adbad2ed7ba9b8c
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-04-19 18:11:38 +0200

    feat: get_children_of_type support recursive

--- src/static/NodeUtils.gd
+++ src/static/NodeUtils.gd
@@ -8,19 +8,19 @@ const FORCE_READABLE_NAME := 1 << 2
 
 # Note: passing a value for the type parameter causes a crash
 static func get_child_of_type(node: Node, child_type):
-	for i in range(node.get_child_count()):
-		var child = node.get_child(i)
+	for child in node.get_children():
 		if is_instance_of(child, child_type):
 			return child
 
 
 # Note: passing a value for the type parameter causes a crash
-static func get_children_of_type(node: Node, child_type) -> Array:
+static func get_children_of_type(node: Node, child_type, recursive: bool = false) -> Array:
 	var list = []
-	for i in range(node.get_child_count()):
-		var child = node.get_child(i)
+	for child in node.get_children():
 		if is_instance_of(child, child_type):
 			list.append(child)
+		if recursive and child.get_child_count() > 0:
+			list.append_array(get_children_of_type(child, child_type, recursive))
 	return list
 
 
