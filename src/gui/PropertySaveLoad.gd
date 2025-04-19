aee7711ef13dbc7b086d62f99e80554c3739896a
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-04-19 17:37:26 +0200

    refactor propertysaveload

--- src/gui/PropertySaveLoad.gd
+++ src/gui/PropertySaveLoad.gd
@@ -3,17 +3,13 @@ extends Node
 class_name PropertySaveLoad
 
 @export var config_property: StringName
-
 @onready var parent := get_parent() as Property
-
-var type: Property.Type
 
 
 func _ready() -> void:
 	parent.value_changed.connect(save)
-	type = parent.type
-	if Config.config.get(config_property) != null:
-		parent.call_deferred("set_value", Config.config.get(config_property), type)
+	if config_property in Config.config:
+		parent.call_deferred("set_value", Config.config.get(config_property))
 
 
 func save(value: Variant) -> void:
