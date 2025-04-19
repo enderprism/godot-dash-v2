4cae962a66ea8c81ccf676e5fc31d574cbf10acc
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-04-19 18:06:15 +0200

    feat: support any node for propertysaveload

--- src/gui/PropertySaveLoad.gd
+++ src/gui/PropertySaveLoad.gd
@@ -3,15 +3,22 @@ extends Node
 class_name PropertySaveLoad
 
 @export var config_property: StringName
+@export var property_owner: Node
 @onready var parent := get_parent() as Property
+var _property_owner: Variant
 
 
 func _ready() -> void:
+	if property_owner != null:
+		_property_owner = property_owner
+	else:
+		_property_owner = Config.config
 	parent.value_changed.connect(save)
-	if config_property in Config.config:
-		parent.call_deferred("set_value", Config.config.get(config_property))
+	if config_property in _property_owner:
+		parent.call_deferred("set_value", _property_owner.get(config_property))
 
 
 func save(value: Variant) -> void:
-	Config.config.set(config_property, value)
-	Config.config.save()
+	_property_owner.set(config_property, value)
+	if _property_owner == Config.config:
+		Config.config.save()
