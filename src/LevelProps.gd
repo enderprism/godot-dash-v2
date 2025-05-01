4e8af65626ab1497db03dfa96b5478f55e801922
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-05-01 16:59:58 +0200

    fix: level song sometimes not loading

--- src/LevelProps.gd
+++ src/LevelProps.gd
@@ -11,7 +11,10 @@ const START_SPEED: Array[float] = [
 	2.431, # 5x
 ]
 
-@export_file var song_path: String
+@export_file var song_path: String:
+	set(value):
+		song_path = value
+		update_song(value)
 @export_range(0.0, 60.0, 0.01, "or_greater", "suffix:s") var song_start_time: float
 @export var platformer: bool
 @export var start_speed: int = 2
@@ -25,19 +28,7 @@ var _pause_manager: Node
 
 func _ready() -> void:
 	_pause_manager = LevelManager.pause_manager
-	if song_path != "":
-		var audio_stream: AudioStream
-		if song_path.begins_with("user"):
-			match song_path.get_extension():
-				"mp3":
-					audio_stream = AudioStreamMP3.load_from_file(song_path)
-				"wav":
-					audio_stream = AudioStreamWAV.load_from_file(song_path)
-				"ogg":
-					audio_stream = AudioStreamOggVorbis.load_from_file(song_path)
-		else:
-			audio_stream = load(song_path)
-		song_player.stream = audio_stream
+	update_song(song_path)
 	song_player.process_mode = Node.PROCESS_MODE_PAUSABLE
 	song_player.set_bus("Music")
 	LevelManager.level_song_player = song_player
@@ -69,3 +60,19 @@ func setup_color_channel_watchers() -> void:
 	for color_channel in color_channels:
 		var watcher := ColorChannelWatcher.new(color_channel)
 		add_child(watcher)
+
+
+func update_song(path: String) -> void:
+	if path.is_empty() or song_player == null:
+		return
+	var audio_stream: AudioStream
+	match song_path.get_extension():
+		"mp3":
+			audio_stream = AudioStreamMP3.load_from_file(path)
+		"wav":
+			audio_stream = AudioStreamWAV.load_from_file(path)
+		"ogg":
+			audio_stream = AudioStreamOggVorbis.load_from_file(path)
+		_:
+			printerr("Song isn't of valid type")
+	song_player.stream = audio_stream
