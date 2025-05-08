f20d5f0d37a014244fdef15d190183e4bbad6c1c
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-05-08 12:58:52 +0200

    fix: togglecomponent can't toggle on or off individually

--- src/interactables/public_components/ToggleComponent.gd
+++ src/interactables/public_components/ToggleComponent.gd
@@ -19,14 +19,21 @@ func toggle(_player: Node):
 	for toggled_group in toggled_groups:
 		var group = toggled_group.group
 		var state = toggled_group.state
+		var skip_deleted := func(object):
+			return not object.is_in_group("deleted")
+		var enable := func(object):
+			object.show()
+			object.process_mode = PROCESS_MODE_INHERIT
+		var disable := func(object):
+			object.hide()
+			object.process_mode = PROCESS_MODE_DISABLED
+		var objects_in_group := get_tree().get_nodes_in_group(group).filter(skip_deleted)
 		if state == ToggledGroup.ToggleState.ON:
-				group.set_deferred_thread_group("process_mode", PROCESS_MODE_INHERIT)
-				group.call_deferred_thread_group("show")
+			objects_in_group.map(enable)
 		elif state == ToggledGroup.ToggleState.OFF:
-				group.set_deferred_thread_group("process_mode", PROCESS_MODE_DISABLED)
-				group.call_deferred_thread_group("hide")
+			objects_in_group.map(disable)
 		elif state == ToggledGroup.ToggleState.FLIP:
-			for object in get_tree().get_nodes_in_group(group):
+			for object in objects_in_group:
 				if object.process_mode == PROCESS_MODE_INHERIT: # If it is toggled on
 					object.set_deferred("process_mode", PROCESS_MODE_DISABLED)
 					object.hide()
