ac1e016f11af5085544d76d244c0bb68f26d9d01
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-05-20 11:15:08 +0200

    property.bool: init

---
+++ rust/src/property/bool.rs
@@ -0,0 +1,72 @@
+use godot::classes::control::SizeFlags;
+use godot::classes::{CheckBox, Control, Engine, HBoxContainer, IHBoxContainer, Label};
+use godot::prelude::*;
+
+use super::Property;
+use crate::expose_property;
+
+#[derive(GodotClass)]
+#[class(init, base=HBoxContainer, tool)]
+struct BoolProperty {
+    // Exports
+    #[export]
+    default: bool,
+
+    // Members
+    #[init(val = OnReady::manual())]
+    label: OnReady<Gd<Label>>,
+    #[init(val = OnReady::manual())]
+    input: OnReady<Gd<CheckBox>>,
+    base: Base<HBoxContainer>,
+}
+
+#[godot_api]
+impl IHBoxContainer for BoolProperty {
+    fn ready(&mut self) {
+        let mut label = Label::new_alloc();
+        let mut spacer = Control::new_alloc();
+        let mut input = CheckBox::new_alloc();
+        let text = self.base().get_name();
+        label.set_text(text.arg());
+        spacer.set_h_size_flags(SizeFlags::EXPAND);
+        input
+            .signals()
+            .toggled()
+            .connect_obj(&self.to_gd(), |s: &mut Self, new_value| {
+                s.signals().value_changed().emit(new_value);
+            });
+        self.base_mut().add_child(&label.clone().upcast::<Node>());
+        self.base_mut().add_child(&spacer.upcast::<Node>());
+        self.base_mut().add_child(&input.clone().upcast::<Node>());
+        self.label.init(label);
+        self.input.init(input);
+        self.signals().renamed().connect_self(Self::refresh);
+        self.refresh();
+    }
+}
+
+expose_property!(BoolProperty, bool);
+impl Property<bool> for BoolProperty {
+    fn set_value(&mut self, value: bool) {
+        self.input.set_pressed_no_signal(value);
+        self.signals().value_changed().emit(value);
+    }
+    fn get_value(&self) -> bool {
+        self.input.is_pressed()
+    }
+    fn reset(&mut self) {
+        self.input.set_pressed_no_signal(self.default);
+    }
+    fn refresh(&mut self) {
+        // Label
+        let text = self.base().get_name();
+        self.label.set_text(text.arg());
+        // Editor refresh
+        if Engine::singleton().is_editor_hint() {
+            self.reset();
+        }
+    }
+    fn set_input_state(&mut self, enabled: bool) {
+        self.input.set_disabled(!enabled);
+    }
+}
--- rust/src/property/mod.rs
+++ rust/src/property/mod.rs
@@ -1,3 +1,4 @@
+pub mod bool;
 pub mod float;
 
 trait Property<T> {
