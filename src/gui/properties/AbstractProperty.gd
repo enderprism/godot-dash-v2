e3c435d860dcd0c480bf5d97ba64ac4890c69aeb
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-06-14 15:44:13 +0200

    abstract base class

---
+++ src/gui/properties/AbstractProperty.gd
@@ -0,0 +1,17 @@
+extends BoxContainer
+class_name AbstractProperty
+
+func set_value(value: Variant) -> void:
+	pass
+
+func get_value() -> Variant:
+	return null
+
+func reset() -> void:
+	pass
+
+func refresh() -> void:
+	pass
+
+func set_input_state(enabled: bool) -> void:
+	pass
