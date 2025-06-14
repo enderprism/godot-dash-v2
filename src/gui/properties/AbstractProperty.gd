05e70c53d24f210db0b9d4f1cf250646ef6727ec
EnderPrism <76617157+enderprism@users.noreply.github.com>
2025-06-14 21:36:10 +0200

    remove typed methods from abstract class

--- src/gui/properties/AbstractProperty.gd
+++ src/gui/properties/AbstractProperty.gd
@@ -1,11 +1,6 @@
 extends BoxContainer
 class_name AbstractProperty
 
-func set_value(value: Variant) -> void:
-	pass
-
-func get_value() -> Variant:
-	return null
 
 func reset() -> void:
 	pass
@@ -15,3 +10,11 @@ func refresh() -> void:
 
 func set_input_state(enabled: bool) -> void:
 	pass
+
+
+func submitted_release_focus(_new_value):
+	get_viewport().gui_release_focus()
+
+func unedit_release_focus(toggled_on):
+	if not toggled_on:
+		get_viewport().gui_release_focus()
