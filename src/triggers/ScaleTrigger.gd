e6f4a5da422d3d0a2bedc3c17531c40703b24bf1
enderprism <76617157+enderprism@users.noreply.github.com>
2025-06-23 23:42:55 +0200

    refactor(scaletrigger): remove redundant variables

--- src/triggers/ScaleTrigger.gd
+++ src/triggers/ScaleTrigger.gd
@@ -10,33 +10,25 @@ enum Mode {
 	COPY,
 }
 
-@export var _scale_around_self: bool:
+@export var scale_around_self: bool:
 	set(value):
-		_scale_around_self = value
+		scale_around_self = value
 		notify_property_list_changed()
-@export var _pivot: Node2D
-@export var _change_position_only: bool
+@export var pivot: Node2D
+@export var change_position_only: bool
 @export var mode: Mode = Mode.ADD:
 	set(value):
 		mode = value
 		notify_property_list_changed()
-@export var _set_scale: Vector2
-@export var _add_scale: Vector2
-@export var _multiply_scale: Vector2
-@export var _copy_target: Node2D
-@export var _copy_multiplier: Vector2 = Vector2.ONE ## Multiplier of the scale of the copy target.
+@export var target_scale: Vector2
+@export var copy_target: Node2D
+@export var copy_multiplier: Vector2 = Vector2.ONE ## Multiplier of the scale of the copy target.
 
 # Hide unneeded elements in the inspector
 func _validate_property(property: Dictionary) -> void:
-	if property.name == "_set_scale" and mode != Mode.SET:
+	if property.name in ["copy_target", "copy_multiplier"] and mode != Mode.COPY:
 		property.usage = PROPERTY_USAGE_NO_EDITOR
-	if property.name == "_add_scale" and mode != Mode.ADD:
-		property.usage = PROPERTY_USAGE_NO_EDITOR
-	if property.name == "_multiply_scale" and mode != Mode.MULTIPLY and mode != Mode.DIVIDE:
-		property.usage = PROPERTY_USAGE_NO_EDITOR
-	if property.name in ["_copy_target", "_copy_multiplier"] and mode != Mode.COPY:
-		property.usage = PROPERTY_USAGE_NO_EDITOR
-	if property.name in ["_pivot", "_change_position_only"] and _scale_around_self:
+	if property.name in ["pivot", "change_position_only"] and scale_around_self:
 		property.usage = PROPERTY_USAGE_NO_EDITOR
 
 var _targets: Array[Node] ## Array of all the [Node2D] in a group.
@@ -55,42 +47,34 @@ func _physics_process(_delta: float) -> void:
 			var _pivot_scale: Vector2
 			# Do assignment outside loop to avoid doing for every object in the group.
 			# The performance benefit is probably negligeable.
-			if not _scale_around_self:
-				_pivot_scale = _pivot.global_scale
+			if not scale_around_self:
+				_pivot_scale = pivot.global_scale
 			for _target: Node2D in _targets:
 				var _initial_global_scale: Vector2 = _initial_scales[_target]
 				var _scale_delta: Vector2
 				match mode:
 					Mode.SET:
-						_scale_delta = (_set_scale - _initial_global_scale) * easing.weight_delta
+						_scale_delta = (target_scale - _initial_global_scale) * easing.weight_delta
 					Mode.ADD:
-						_scale_delta = _add_scale * easing.weight_delta
+						_scale_delta = target_scale * easing.weight_delta
 					Mode.MULTIPLY:
-						_scale_delta = ((_initial_global_scale * _multiply_scale) - _initial_global_scale) * easing.weight_delta
+						_scale_delta = ((_initial_global_scale * target_scale) - _initial_global_scale) * easing.weight_delta
 					Mode.DIVIDE:
-						_scale_delta = ((_initial_global_scale / _multiply_scale) - _initial_global_scale) * easing.weight_delta
+						_scale_delta = ((_initial_global_scale / target_scale) - _initial_global_scale) * easing.weight_delta
 					Mode.COPY:
-						if _copy_target != null:
-							_target.global_scale = lerp(_initial_global_scale, _copy_target.global_scale * _copy_multiplier, easing.weight)
+						if copy_target != null:
+							_target.global_scale = lerp(_initial_global_scale, copy_target.global_scale * copy_multiplier, easing.weight)
 						elif LevelManager.in_editor and LevelManager.level_playing:
 							printerr("In ", name, ": copy_target is unset!")
 						# Escape the current loop iteration to avoid adding the rotation delta, even if it's null.
 						continue
-				if _scale_around_self:
+				if scale_around_self:
 					_target.global_scale += _scale_delta
 				else:
-					# var _target_parent: Node = _target.get_parent()
-					# _pivot.global_scale = _initial_global_scale
-					# _target.reparent(_pivot)
-					# _pivot.global_scale += _scale_delta
-					# _target.reparent(_target_parent)
-					# _pivot.global_scale = _pivot_scale
-					# if _change_position_only:
-					# 	_target.global_scale = _initial_global_scale
-					var position_relative_to_pivot: Vector2 = _target.global_position - _pivot.global_position
+					var position_relative_to_pivot: Vector2 = _target.global_position - pivot.global_position
 					var position_delta := position_relative_to_pivot * ((_target.global_scale + _scale_delta) / _target.global_scale) - position_relative_to_pivot
 					_target.global_position += position_delta
-					if not _change_position_only:
+					if not change_position_only:
 						_target.global_scale += _scale_delta
 		elif LevelManager.in_editor and LevelManager.level_playing:
 			printerr("In ", name, "_process: _target is unset")
