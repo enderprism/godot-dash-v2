26588a730543b6e9b3ebf8c9fb41d11d964a3fe8
enderprism <76617157+enderprism@users.noreply.github.com>
2025-07-24 10:45:38 +0200

    finish resourceproperty implementation

--- src/gui/properties/types/ResourceProperty.gd
+++ src/gui/properties/types/ResourceProperty.gd
@@ -12,10 +12,12 @@ signal value_changed(value: Resource)
 			push_error("Resource script does not extend Resource")
 		resource_script = value
 		default = value.new()
+		default.resource_local_to_scene = true
 @export_tool_button("Refresh") var _refresh = refresh
 
 var label: Label
 var indentation_container: VBoxContainer
+var resource_properties: Array
 
 
 func _ready() -> void:
@@ -31,10 +33,23 @@ func _ready() -> void:
 		"VBoxContainer",
 		VBoxContainer,
 		NodeUtils.INTERNAL)
+	resource_properties = resource_script.get_script_property_list()
+	resource_properties.remove_at(0)
+	resource_properties = resource_properties \
+			.filter(func(property): return property["usage"] & PROPERTY_USAGE_EDITOR != 0) \
+			.map(func(property): return property["name"])
 	for child in get_children(false):
 		child.hide()
 		var child_duplicate = child.duplicate()
 		child_duplicate.show()
+		child_duplicate.value_changed.connect(func(value):
+			if child_duplicate is Node2DProperty:
+				if LevelManager.editor_edited_level == null:
+					value = ^""
+				else:
+					value = LevelManager.editor_edited_level.get_path_to(value)
+			_value.set(resource_properties[child_duplicate.get_index()], value)
+			value_changed.emit(_value))
 		indentation_container.add_child(child_duplicate)
 	renamed.connect(refresh)
 	refresh()
@@ -45,3 +60,27 @@ func refresh() -> void:
 	label.custom_minimum_size.y = custom_minimum_size.y
 	label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
 	vertical = true
+	if Engine.is_editor_hint():
+		reset()
+
+
+func set_value(new_value: Resource) -> void:
+	set_value_no_signal(new_value)
+	value_changed.emit(new_value)
+
+func set_value_no_signal(new_value: Resource) -> void:
+	_value = new_value
+	for i in indentation_container.get_child_count():
+		var field_input = indentation_container.get_child(i)
+		var field_name = resource_properties[i]
+		var field_value = resource_script.get(field_name)
+		field_input.set_value(field_value)
+
+func get_value() -> Resource:
+	return _value
+
+func reset() -> void:
+	set_value(default)
+
+func set_input_state(enabled: bool) -> void:
+	indentation_container.get_children().map(func(input): input.set_input_state(enabled))
