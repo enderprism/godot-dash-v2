d68bf271d25aece63da52ac0bea8eee455b04fbf
enderprism <76617157+enderprism@users.noreply.github.com>
2025-08-04 20:52:33 +0200

    save and load fields in interactable editor; handle bool fields

--- src/editor/InteractableEditor.gd
+++ src/editor/InteractableEditor.gd
@@ -11,30 +11,32 @@ func build_ui(interactables: Array[Interactable]) -> void:
 		fields.remove_at(0)
 		fields = fields \
 				.filter(func(field): return field["usage"] & PROPERTY_USAGE_EDITOR != 0)
-		print_debug(component, ": ", fields)
 		for field in fields:
 			var field_name: String = field["name"]
 			if field_name.begins_with("_"):
 				continue
 			var field_value = component.get(field_name)
+			var property: AbstractProperty
 			match typeof(field_value):
 				TYPE_INT:
 					if field["hint"] == PROPERTY_HINT_ENUM:
-						var property := EnumProperty.new()
-						property.name = field_name.to_pascal_case()
+						property = EnumProperty.new()
 						property.fields = field["hint_string"].split(",")
 						for i in property.fields.size():
 							property.fields.set(i, property.fields[i].get_slice(":", 0))
 						ui_root.add_child(property)
+				TYPE_BOOL:
+					property = BoolProperty.new()
+			property.name = field_name.to_pascal_case()
+			property.set_meta("component_name", component.name)
 	$MarginContainer.add_child(ui_root)
 
-	# connect_ui(interactables)
-	# load_properties.call_deferred(first_interactable)
+	connect_ui(interactables, ui_root)
+	load_properties.call_deferred(first_interactable, ui_root)
 
 
-func connect_ui(interactables: Array[Interactable]) -> void:
-	var properties := NodeUtils.get_children_of_type(%UIRoot, AbstractProperty, true) \
-			.filter(func(node): return not node.owner.is_queued_for_deletion())
+func connect_ui(interactables: Array[Interactable], ui_root: Control) -> void:
+	var properties := NodeUtils.get_children_of_type(ui_root, AbstractProperty, true)
 	if properties.is_empty():
 		return
 	for property in properties as Array[AbstractProperty]:
@@ -43,28 +45,24 @@ func connect_ui(interactables: Array[Interactable]) -> void:
 				property.value_changed.disconnect(connection.callable)
 		property.value_changed.get_connections().map(remove_connections)
 		var property_name := property.name.to_snake_case()
-		if property.has_node("TriggerPropertyInternalName"):
-			property_name = property.get_node("TriggerPropertyInternalName").property_name
-		property.value_changed.connect(save_property.bind(property_name, interactables))
+		property.value_changed.connect(save_property.bind(property.get_meta("component_name"), property_name, interactables))
+		print(property.value_changed.get_connections())
 
 
-func save_property(value: Variant, property: String, interactables: Array[Interactable]) -> void:
-	if property == "target_group":
-		value = GroupEditor.GROUP_PREFIX + value
-	interactables.map(func(interactable): interactable.set(property, value))
+func save_property(value: Variant, component_name: String, property: String, interactables: Array[Interactable]) -> void:
+	prints(property, value, interactables)
+	interactables.map(func(interactable): interactable.get_node(component_name).set(property, value))
 
 
-func load_properties(interactable: Interactable) -> void:
-	var properties := NodeUtils.get_children_of_type(%UIRoot, AbstractProperty, true) \
-			.filter(func(node): return not node.owner.is_queued_for_deletion())
+func load_properties(interactable: Interactable, ui_root: Control) -> void:
+	var properties := NodeUtils.get_children_of_type(ui_root, AbstractProperty, true)
 	if properties.is_empty():
 		return
 	for property in properties as Array[AbstractProperty]:
 		var property_name := property.name.to_snake_case()
-		if property.has_node("TriggerPropertyInternalName"):
-			property_name = property.get_node("TriggerPropertyInternalName").property_name
-		if interactable.get(property_name) == null:
+		var component := interactable.get_node(String(property.get_meta("component_name")))
+		if component.get(property_name) == null:
 			printerr("Can't load property ", property_name, " on ", interactable)
 			continue
-		var value = interactable.get(property_name)
+		var value = component.get(property_name)
 		property.set_value_no_signal(value)
