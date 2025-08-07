7e54dbed7df7f28c99ed0ce66912c92fd47f266f
enderprism <76617157+enderprism@users.noreply.github.com>
2025-08-07 18:19:19 +0200

    revert dc19ccd

--- src/interactables/public_components/JumpBoostComponent.gd
+++ src/interactables/public_components/JumpBoostComponent.gd
@@ -1,8 +1,8 @@
 extends Component
 class_name JumpBoostComponent
 
-@export var _jump_boost: float
+@export var jump_boost: float
 
 
 func get_velocity(player: Player) -> float:
-	return _jump_boost * -player.speed.y * player.gameplay_trigger_gravity_multiplier * sign(player.gravity_multiplier)
+	return jump_boost * -player.speed.y * player.gameplay_trigger_gravity_multiplier * sign(player.gravity_multiplier)
