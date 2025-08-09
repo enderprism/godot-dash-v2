08c7a135bc307fe93838bbf78b3f33a910020f88
enderprism <76617157+enderprism@users.noreply.github.com>
2025-08-09 09:33:32 +0200

    feat: error toasts

--- src/autoloads/Toasts.gd
+++ src/autoloads/Toasts.gd
@@ -3,16 +3,17 @@ extends Node
 const TOAST_PACKED := preload("res://scenes/components/game_components/Toast.tscn")
 const TOAST_LAYER_PACKED := preload("res://scenes/components/game_components/ToastLayer.tscn")
 
-enum ToastOptions {
+enum {
 	NONE = 0,
 	PERSISTENT = 1, # Doesn't disappear after a delay, the user must click on it to dismiss it.
+	ERROR = 1 << 2,
 }
 
 var toast_layer: CanvasLayer
 var toast_container: VBoxContainer
 
 
-func new_toast(text: String, duration: float = 1.0, options: int = ToastOptions.NONE) -> Toast:
+func new_toast(text: String, duration: float = 1.0, options: int = NONE) -> Toast:
 	if not get_tree().root.has_node("ToastLayer"):
 		toast_layer = TOAST_LAYER_PACKED.instantiate() as CanvasLayer
 		get_tree().root.add_child(toast_layer, true)
@@ -22,6 +23,8 @@ func new_toast(text: String, duration: float = 1.0, options: int = ToastOptions.
 	var toast := TOAST_PACKED.instantiate() as PanelContainer
 	toast.text = text
 	toast.lifetime = duration
-	toast.persistent = options & ToastOptions.PERSISTENT
+	toast.persistent = options & PERSISTENT
+	if options & ERROR:
+		toast.modulate = Color.DEEP_PINK
 	toast_container.add_child(toast)
 	return toast
