85b19c6e0d942a07458eb353fdfa8a7fae2c9c68
enderprism <76617157+enderprism@users.noreply.github.com>
2025-08-22 09:52:05 +0200

    feat: required components

--- src/interactables/Component.gd
+++ src/interactables/Component.gd
@@ -6,3 +6,11 @@ class_name Component
 
 func _ready() -> void:
 	parent.register_public(self)
+
+
+func require(component_types: Array[Script]) -> void:
+	for component_type in component_types:
+		if not parent.has(component_type):
+			push_warning("%s is missing %s" % [parent, component_type.get_global_name()])
+			var component_instance: Component = component_type.new()
+			parent.add_child(component_instance, true)
